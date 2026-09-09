df555641fd251da198eb4a4ea4df817d2429b658
Andi Angel <andi.angel@boheemia.ee>
2026-09-09 19:05:02 +0300

    Fix build

--- src/test/java/ee/smit/infodesk/InfodeskApplicationTests.java
+++ src/test/java/ee/smit/infodesk/InfodeskApplicationTests.java
@@ -1,10 +1,15 @@
 package ee.smit.infodesk;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class InfodeskApplicationTests {
+
+	@MockitoBean
+	ChatModel chatModel;
 
 	@Test
 	void contextLoads() {
