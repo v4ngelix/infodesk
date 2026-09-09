61165c0a682ce47e5ed027c50c95affb5ec21867
Andi Angel <andi.angel@boheemia.ee>
2026-09-09 17:15:35 +0300

    Initial commit

---
+++ src/test/java/ee/smit/infodesk/InfodeskApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.smit.infodesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfodeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
